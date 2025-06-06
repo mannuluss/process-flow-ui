3c5914c6fd986be206d72db8e08338f953b61a54
Felipe Rojas <andres2180021@correo.uis.edu.co>
2025-06-06 12:13:14 -0500

    fix:  fitView is called after loadDataCommand

--- src/app/actions/commands/load.config.command.ts
+++ src/app/actions/commands/load.config.command.ts
@@ -3,4 +3,5 @@ import { CommandHandler } from "@commands/interfaces/command.interfaces";
 export const loadDataCommand: CommandHandler = (context) => {
   context.state.setNodes(context.object?.nodes ?? []);
   context.state.setEdges(context.object?.connections ?? []);
+  context.state.fitView();
 };
