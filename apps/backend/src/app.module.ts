456ea015f854c1aacc668c18684165a7bce8f0dd
Mannulus <pipe_050401@hotmail.com>
2026-01-25 18:29:07 -0500

    fix typeorm migrations initial

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -9,6 +9,8 @@ import { ProcessInstance } from './process-instance/entities/process-instance.en
 import { DataSource } from './data-source/entities/data-source.entity';
 import { ProcessInstanceModule } from './process-instance/process-instance.module';
 import { DataSourceModule } from './data-source/data-source.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { EngineModule } from './engine/engine.module';
 
 @Module({
@@ -21,13 +23,20 @@ import { EngineModule } from './engine/engine.module';
       username: process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD || 'postgres',
       database: process.env.DB_NAME || 'process_flow',
+      schema: process.env.DB_SCHEMA || 'public',
       entities: [Workflow, ProcessInstance, DataSource],
-      synchronize: false, // We are using migrations
+      migrations: [join(__dirname, '..', 'migrations', '*{.ts,.js}')],
+      migrationsRun: process.env.DB_SYNCHRONIZE !== 'true',
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
     }),
     WorkflowModule,
     ProcessInstanceModule,
     DataSourceModule,
     EngineModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'public'),
+      exclude: ['/api/(.*)'],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/backend/typeorm-cli.config.ts
+++ apps/backend/typeorm-cli.config.ts
@@ -10,6 +10,7 @@ export default new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  schema: process.env.DB_SCHEMA || 'public',
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['migrations/*{.ts,.js}'],
   synchronize: false,
